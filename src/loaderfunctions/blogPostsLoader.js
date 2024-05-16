cdf1af8aaa1fbe2a031ac8f0dafcfa54d702f9fc
Obinna Anosike <smartlify09@gmail.com>
2024-05-16 14:13:58 +0100

    Added Json-server url

--- src/loaderfunctions/blogPostsLoader.js
+++ src/loaderfunctions/blogPostsLoader.js
@@ -1,5 +1,7 @@
 export const blogPostsLoader = async () => {
-  const response = await fetch("http://localhost:4001/posts");
+  const response = await fetch(
+    "https://json-server-vercel-three-steel.vercel.app/posts"
+  );
   const posts = await response.json();
   return posts;
 };
