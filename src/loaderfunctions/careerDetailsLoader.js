96d3409b1ac2197ecda213e91bee976a43704c49
Obinna Anosike <smartlify09@gmail.com>
2024-05-16 14:15:04 +0100

    Added Json-server url for the careers

--- src/loaderfunctions/careerDetailsLoader.js
+++ src/loaderfunctions/careerDetailsLoader.js
@@ -1,7 +1,9 @@
 export const careerDetailsLoader = async ({ params }) => {
   const { id } = params;
 
-  const response = await fetch("http://localhost:4000/careers/" + id);
+  const response = await fetch(
+    "https://json-server-vercel-three-steel.vercel.app/careers/" + id
+  );
   if (!response.ok) {
     throw Error("Oops couldn't find that career 😶");
   }
