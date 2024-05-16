0f7e2cec49c742c229257db0b51210eaaa500706
Obinna Anosike <smartlify09@gmail.com>
2024-05-16 16:03:31 +0100

    Updated post detail loader

--- src/loaderfunctions/postDetailLoader.js
+++ src/loaderfunctions/postDetailLoader.js
@@ -1,6 +1,8 @@
 export const postDetailLoader = async ({ params }) => {
   const { id } = params;
-  const response = await fetch("http://localhost:4001/posts/" + id);
+  const response = await fetch(
+    "https://json-server-vercel-three-steel.vercel.app/posts/" + id
+  );
   if (!response.ok) {
     throw Error("Oops! 😶 couldn't find that Blog ");
   }
